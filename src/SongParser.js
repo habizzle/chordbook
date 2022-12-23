e72d57ed92bca069b7c88dc58b112a026a0c11ec
hannesb <hb.hannes.becker@gmail.com>
2022-12-23 07:06:11 +0100

    Init

---
+++ src/SongParser.js
@@ -0,0 +1,73 @@
+import YAML from 'yaml';
+import fs from 'fs';
+import path from 'path';
+import {transposeLine} from "./Transposer.js";
+
+export const parse = (path, key) => {
+    return findYamlSongDocs(path)
+        .map((filePath) => bookify(filePath, key));
+};
+
+const findYamlSongDocs = (directory) => {
+    return fs.readdirSync(directory, {withFileTypes: true})
+        .filter((it) => it.isFile())
+        .map((it) => it.name)
+        .filter((it) => it.endsWith(".yml"))
+        .map((it) => path.join(directory, it));
+}
+
+const bookify = (filePath, key) => ({
+    title: formatTitle(filePath),
+    songs: parseYamlSongDocs(filePath).map((songDoc) => songify(songDoc, key))
+});
+
+const formatTitle = (filePath) => filePath
+    .replace(/^books\//, '')
+    .replace(/(.private)?.yml$/, '')
+    .replace('-', ' ');
+
+const parseYamlSongDocs = (filePath) => {
+    const file = fs.readFileSync(filePath, 'utf8');
+    const docs = YAML.parseAllDocuments(file);
+    console.log(`Finished parsing ${docs.length} songs from ${filePath}`);
+    return docs;
+}
+
+const songify = (songDoc, key) => {
+    let verseCounter = 1;
+    const docObj = songDoc.toJS();
+
+    const blockify = (blockDoc) => {
+        const refrain = blockDoc.tag === '!Ref';
+        const name = !refrain
+            ? blockDoc.tag?.slice(1) ?? `Verse ${verseCounter++}`
+            : 'Refrain';
+        const lines = linify(blockDoc.value.trimEnd());
+        return {
+            refrain: refrain,
+            name: name,
+            lines: lines
+        };
+    };
+
+    const linify = (block) => block.split("\n").map(line => {
+        const chord = line.indexOf("  ") >= 0 // either chords are separated by more than 1 whitespace
+            || line.indexOf(" ") < 0; // or line contains just 1 chord - it is assumed a line never contains of just one word
+        if (chord) {
+            if (key) {
+                line = transposeLine(line, docObj.key, key);
+            }
+        }
+        return {
+            type: chord ? 'chord' : 'text',
+            content: line
+        }
+    });
+
+    const blocks = songDoc.contents.get("song").items.map(blockDoc => blockify(blockDoc));
+    return {
+        title: docObj.title,
+        author: docObj.author,
+        blocks: blocks,
+    };
+};
---
+++ src/Transposer.js
@@ -0,0 +1,59 @@
+export const transposeLine = (line, oldKey, newKey) => {
+    // if unset, use default
+    if (!oldKey) {
+        oldKey = 'C';
+    }
+
+    if (oldKey === newKey) {
+        return line;
+    }
+
+    const oldKeyIndex = transpositions[0]
+        .findIndex(item => item === oldKey);
+
+    let newKeyIndex = transpositions[0]
+        .findIndex(item => item === newKey);
+
+    if (oldKeyIndex < 0 || newKeyIndex < 0) {
+        throw new Error(`Unsupported transpose from ${oldKey} to ${newKey}`);
+    }
+
+    let modifiedLine = intelliLine(line);
+
+    transpositions
+        .forEach(item => modifiedLine = modifiedLine.transpose(item[oldKeyIndex], item[newKeyIndex]));
+
+    return modifiedLine.toLine();
+}
+
+const transpositions = [
+    ["C", "G"],
+    ["Cadd9", "Gadd9"],
+    ["A", "E"],
+    ["Am", "Em"],
+    ["F", "C"],
+    ["G", "D"],
+    ["G7", "D7"],
+    ["D", "A"],
+    ["Dm", "Am"],
+    ["E", "B"],
+    ["E7", "B7"],
+    ["Em", "Bm"],
+    ["Em7", "Bm7"],
+
+]
+
+const intelliLine = (line) => {
+    // Finds last chord in line ("${sourceChord}$")
+    // and chord which is followed by another chord, separated by whitespace ("${sourceChord} ")
+    const transposeRegex = (sourceChord) => new RegExp(`(${sourceChord}$|${sourceChord} )`, "g");
+
+    return {
+        transpose: (oldKey, newKey) => {
+            const transposedLine = line.replace(transposeRegex(oldKey), `${newKey}$ `);
+            return intelliLine(transposedLine);
+        },
+        toLine: () => line.replace(/\$/g, "")
+    }
+}
+
